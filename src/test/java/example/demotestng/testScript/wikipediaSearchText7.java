b330b59efe567f8cd48cdf536d8a61631d756d71
Hardik Duva <hardik.duva@halight.com>
2024-06-25 15:55:12 +0530

    Update project

---
+++ src/test/java/example/demotestng/testScript/wikipediaSearchText7.java
@@ -0,0 +1,20 @@
+package example.demotestng.testScript;
+
+import example.demotestng.pages.BasePage;
+import example.demotestng.pages.wikipedia.SearchPage;
+import org.testng.annotations.Test;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class wikipediaSearchText7 extends BasePage {
+
+    @Test
+    void shouldSearchResult() {
+        SearchPage searchPage = new SearchPage(page);
+        searchPage.navigate();
+        searchPage.search("junit");
+        assertThat(page).hasTitle(Pattern.compile("JUnit"));
+    }
+}
